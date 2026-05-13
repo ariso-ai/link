8a5a2691cc560ab802acefc811c064093932349d
michaelgeiger <michael.geiger@ariso.ai>
2026-05-13 13:59:19 -0400

    fix index imports

--- nango.dev/index.ts
+++ nango.dev/index.ts
@@ -1,22 +1,22 @@
 // Jira integrations
-import ./jira/actions/list-issues.js;
-import ./jira/actions/create-issue.js;
+import './jira/actions/list-issues.js';
+import './jira/actions/create-issue.js';
 
 // Confluence integrations
-import ./confluence/actions/search-confluence.js;
-import ./confluence/actions/get-page.js;
-import ./confluence/actions/list-spaces.js;
-import ./confluence/actions/get-page-children.js;
+import './confluence/actions/search-confluence.js';
+import './confluence/actions/get-page.js';
+import './confluence/actions/list-spaces.js';
+import './confluence/actions/get-page-children.js';
 
 // Linear integrations
-import ./linear/actions/list-issues.js;
-import ./linear/actions/get-issue.js;
-import ./linear/actions/update-issue.js;
-import ./linear/actions/add-comment.js;
+import './linear/actions/list-issues.js';
+import './linear/actions/get-issue.js';
+import './linear/actions/update-issue.js';
+import './linear/actions/add-comment.js';
 
 // QuickBooks integrations
-import ./quickbooks/actions/run-report.js;
-import ./quickbooks/actions/list-customers.js;
-import ./quickbooks-sandbox/actions/list-customers.js;
-import ./quickbooks/actions/create-journal-entry.js;
-import ./quickbooks-sandbox/actions/create-journal-entry.js;
+import './quickbooks/actions/run-report.js';
+import './quickbooks/actions/list-customers.js';
+import './quickbooks-sandbox/actions/list-customers.js';
+import './quickbooks/actions/create-journal-entry.js';
+import './quickbooks-sandbox/actions/create-journal-entry.js';
